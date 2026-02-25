e4578015507faad4603d418a0b9438e89f9451f6
bosseschum <accounts@bschum.de>
2026-02-25 13:21:30 +0100

    Implemented shortest_path

--- lib/knight.rb
+++ lib/knight.rb
@@ -3,8 +3,39 @@
 # Knight class representing the chess figure.
 # Computes the shortest path of valid moves from one position to another.
 class Knight
-  def shortest_path(start, _goal)
+  def knight_moves(start, goal)
+    path = shortest_path(start, goal)
+    puts "You made it in #{path.length} moves! Here's your path:"
+    print path
+  end
+
+  def shortest_path(start, goal)
     nil if out_of_bounds?(start)
+
+    queue = [start]
+    visited = [start]
+    path = {}
+    result = []
+
+    until queue.empty?
+      current = queue.shift
+      break if current == goal
+
+      valid_moves(current).each do |pos|
+        next if visited.include?(pos)
+
+        queue << pos
+        visited << pos
+        path[pos] = current
+      end
+    end
+    current = goal
+    until current == start
+      result << current
+      current = path[current]
+    end
+    result << start
+    result.reverse
   end
 
   def out_of_bounds?(position)
---
+++ main.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require_relative 'lib/knight'
+
+knight = Knight.new
+
+knight.knight_moves([3, 3], [4, 3])
